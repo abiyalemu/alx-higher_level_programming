11b5b17b89f831815d9c89b509eb320461726ad4
root <abiya.hiwot@gmail.com>
2021-11-16 08:09:30 -0800

    python script

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "Helberton School"
+print("{}{}{}\n{:.9}".format(str, str, str, str))
---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "python is an interpreted, interactive, object-oriented programming language that combine remarkable power with very clear syntax"
+str = str[36:66] + str[106:112]  + str[:6]
+print(str)
