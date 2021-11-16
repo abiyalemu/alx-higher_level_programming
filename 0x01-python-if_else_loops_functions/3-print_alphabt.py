eaa1c790a34dce1e1ffd94ade83f86a03482431e
root <abiya.hiwot@gmail.com>
2021-11-16 12:24:04 -0800

    printing ASCLL exclude q, e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for k in range(ord('a'), ord('z'), + 1):
+for k in range(ord("a"), ord("z"), + 1):
     if chr(k) != "q" and chr(k) != "e":
         print("{:s}".format(chr(k)), end="")
