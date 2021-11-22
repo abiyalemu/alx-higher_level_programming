a470c30effa8573d84b925324f4090d8c73d0e70
root <abiya.hiwot@gmail.com>
2021-11-22 09:45:57 -0800

    9 max intger and 10 divisble by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = my_list[:]
+    for k in range(0, len(my_list)):
+        if my_list[k] % 2 == 0:
+            new_list[k] = True
+        else:
+            new_list[k] = False
+    return new_list
