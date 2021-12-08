56d1768bca75eefdef1569367fb16561700933e1
root <abiya.hiwot@gmail.com>
2021-12-08 03:36:25 -0800

    class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class of documentation"""
+
+
+class Square():
+    """A quadrilateral with four equal sides for square class"""
+
+    def __init__(self, size):
+        """private instance size of the new square object sets
+        Args:
+            size (int): the size of the square once instance is created
+        """
+        self.__size = size
