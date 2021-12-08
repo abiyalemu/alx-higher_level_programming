5cb801a8a19fd2490e3949c1e3971e16c002fb09
root <abiya.hiwot@gmail.com>
2021-12-07 23:44:13 -0800

    creating empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
+"""Documentation for a square class"""
 
 class Square():
-    """Square class of a quadrilateral with four equal sides
-       Attributes:
-           None
-    """
+    """Empty class"""
     pass
