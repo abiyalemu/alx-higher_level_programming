b4fb37293b4e807e13bfb9aab1121643216fc0e5
root <abiya.hiwot@gmail.com>
2022-03-01 02:32:04 -0800

    languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-// prints lines
-
-const firstLine = 'C if fun';
-const secondLine = 'python is cool';
-const thirdLine = 'Javascript is amazing';
-
-console.log(firstLine + '\n' + secondLine + '\n' + thirdLine);
-
+console.log('C is fun\nPython is cool\nJavascript is amazing');
